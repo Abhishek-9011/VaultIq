f80599049eac400a18185add20275c0dcb8ddcf8
Abhishek singh <abhishek774901@gmail.com>
2025-06-07 23:06:38 +0530

    token file deleted

--- Backend/src/config/Token.ts
+++
@@ -1,2 +0,0 @@
-// export const JWT_PASSWORD = "123djfkldsjfklas4334ilknsfglksfj";
-// export const MONGO_DB_URL = "mongodb+srv://abhishek774901:Jq3k8nyWi9zfxtS9@cluster0.xt6sa.mongodb.net/VaultIq";
