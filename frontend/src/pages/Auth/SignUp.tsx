c5a7d3c1556a2ef25e4d5d1225da6e0af484dfe6
Abhishek singh <abhishek774901@gmail.com>
2026-05-06 15:25:16 +0530

    changes for deployment

--- frontend/src/pages/Auth/SignUp.tsx
+++ frontend/src/pages/Auth/SignUp.tsx
@@ -1,6 +1,5 @@
 import { useRef } from "react";
-import { Input } from "../../components/ui/Input";
-import { Button } from "../../components/ui/Buttons";
+
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
